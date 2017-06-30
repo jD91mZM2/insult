8f913b4b9a56a54b219872b5b2ff76f9cfe12403
LEGOlord208 <LEGOlord208@krake.one>
2017-06-30 16:24:23 +0200

    Improved install-flavor

--- install-flavor.sh
+++ install-flavor.sh
@@ -1,32 +1,65 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-	echo "./install-flavor.sh <branch> [destination]"
+	echo "./install-flavor.sh <branch> [--merge] [destination]"
 	exit 1
 fi
 
-dest=~/.config/insult
-if [ -n "$2" ]; then
-	dest="$2"
+branch="$1"
+dest="$2"
+merge=false
+
+if [ "$dest" == "--merge" ]; then
+	dest="$3"
+	merge=true
+fi
+if [ -z "$dest" ]; then
+	dest=~/.config/insult
 fi
 
-echo -n "You are about to replace your insult config files. Continue? (y/n)"
-read -n 1 answer
-echo
-if [ "$answer" != "y" ]; then
-	echo -n "Answer is not 'y'. Exitting!"
-	exit 1
-fi
+if [ "$merge" == "false" ]; then
+	echo "--merge not set."
+	echo "Warning: You are about to completely replace your insult config files."
+	echo -n "Continue? (y/n) "
+	read -n 1 answer
+	echo
+	if [ "$answer" != "y" ]; then
+		echo -n "Answer is not 'y'. Exitting!"
+		exit 1
+	fi
 
-if [ "$1" == "master" ]; then
 	rm -v "$dest/nouns"
 	rm -v "$dest/endings"
 	rm -v "$dest/verbs"
+
+	if [ "$branch" == "master" ]; then
+		echo "Executing insult to regenerate files..."
+		(
+			(which insult && insult) ||
+			(stat ./target/release/insult && ./target/release/insult) ||
+			find . -name insult -executable -type f -exec "{}" \; ||
+			(echo "Can't guess where insult lives." && exit)
+		) &> /dev/null
+		exit
+	fi
+fi
+
+if [ "$branch" == "master" ]; then
+	echo "Branch master and --merge are not compatible. Exitting."
 	exit
 fi
 
-git checkout "$1" &> /dev/null
-cp -v nouns "$dest/nouns"
-cp -v endings "$dest/endings"
-cp -v verbs "$dest/verbs"
+git stash > /dev/null # In case of any uncommited changes
+
+git checkout "$branch" &> /dev/null
+
+printf "\n# %s\n" "$branch" >> "$dest/nouns"
+printf "\n# %s\n" "$branch" >> "$dest/endings"
+printf "\n# %s\n" "$branch" >> "$dest/verbs"
+cat nouns   >> "$dest/nouns"   && echo "\"nouns\"   -> \"$dest/nouns\""
+cat endings >> "$dest/endings" && echo "\"endings\" -> \"$dest/endings\""
+cat verbs   >> "$dest/verbs"   && echo "\"verbs\"   -> \"$dest/verbs\""
+
 git checkout master &> /dev/null
+
+git stash pop > /dev/null
