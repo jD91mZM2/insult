e61157064897e27c284d622ab3841d606cfaf2cb
jD91mZM2 <me@krake.one>
2017-10-27 15:12:31 +0200

    Delete scripts because they're useless

--- install-flavor-all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# ./install-flavor-all [config folder]
-./install-flavor.sh master "$1"
-./install-flavor.sh flavor-arrowverse --merge "$1"
-./install-flavor.sh flavor-meme --merge "$1"
